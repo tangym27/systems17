a5ed5273012b7c198dd597f1674e9b11455ecabe
Michelle Tang <mtang1@stuy.edu>
2018-12-14 21:41:01 -0500

    whoops

---
+++ client.c
@@ -0,0 +1,66 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <fcntl.h>
+#include <unistd.h>
+#include <string.h>
+#include <errno.h>
+#include <sys/types.h>
+#include <sys/ipc.h>
+#include <sys/sem.h>
+#include <sys/shm.h>
+#define KEY 314159
+#define KEY2 271
+
+
+int main() {
+  int sem_id=semget(KEY,1,0644);
+  int * count;
+  struct sembuf sem1;
+
+// DOWN THE SEMPAHORE
+  sem1.sem_flg=SEM_UNDO;
+  sem1.sem_num=0;
+  sem1.sem_op=-1;
+  semop(sem_id,&sem1,1);
+
+  int fd=open("story.txt",O_RDWR | O_APPEND,0644);
+
+  if (fd==-1) { //in case theres an error, restart/reup everything
+    printf("Oh no! There was an error when opening: %s\n",strerror(errno));
+    sem1.sem_op=1;
+    semop(sem_id,&sem1,1);
+    return 0;
+  }
+
+  int shmid=shmget(KEY2,sizeof(int),IPC_CREAT | IPC_EXCL | 0644);
+  char last_line[256];
+  if (shmid==-1) {//bc of excl
+    shmid=shmget(KEY2,sizeof(int),IPC_CREAT | 0644); //re shmid!
+    count=shmat(shmid,0,0);
+
+    //start reading at the end
+    lseek(fd,-1*(*count),SEEK_END);
+    read(fd,last_line,*count);
+    printf("Last line:\n%s",last_line);
+  }
+  else { // very first time!
+    printf("Last line: N/A\n");
+    count=shmat(shmid,0,0);
+  }
+
+// WRITING TO THE FILE
+  char message[256];
+  printf("What would you like the next line to be? ");
+  fgets(message,256,stdin);
+  *count=strlen(message);
+  shmdt(count);
+
+  write(fd,message,strlen(message));
+  close(fd);
+
+// UP THE SEMAPHORE!
+  sem1.sem_op=1;
+  semop(sem_id,&sem1,1);
+
+  return 0;
+}
--- control.c
+++ control.c
@@ -1,71 +1,96 @@
-#include <stdlib.h>
 #include <stdio.h>
-#include <string.h>
-#include <unistd.h>
-#include <dirent.h>
+#include <stdlib.h>
 #include <sys/types.h>
 #include <sys/ipc.h>
-#include <sys/shm.h>
-#include <errno>
 #include <sys/sem.h>
+#include <string.h>
+#include <unistd.h>
+#include <errno.h>
+#include <fcntl.h>
+#include <sys/shm.h>
+#define KEY 314159
+#define SHM_KEY 38
 
-#define SHM_SIZE 500;
-#define KEY 0xRINGING
-
-union semun{
-	int val;
-	struct semid_ds *buf;
-	unsigned short *array;
-	struct seminfo*_buf;
-}
 
 void create(){
-	int shmid = shmget(KEY,SHM_SIZE, IPC_CREAT | IPC_EXCL | 0644);
-	if(shmid < 0){
-		printf("Error %s\n",strerror(errno));
-	}
-	int fd = open("story.txt",O_CREAT | O_TRINC | O_EXCL);
-	if(fd < 0){
-		printf("Error %s\n",strerror(errno));
-	}
-	int semid = semget(KEY,1 IPC_CREAT | IPC_EXCL | 0644);
-	union semun us;
-	if(semid < 0){
-		printf("Error %S\n",strerror(errno));
-	}
+  printf("You have chosen to CREATE the story\n");
+  union semun semaphore;
+  int sem_id=semget(KEY,1,IPC_CREAT | IPC_EXCL | 0600);
+  if (sem_id>=0) {
+    semaphore.val=1;
+    semctl(sem_id,0,SETVAL,semaphore.val);
+    int fd=open("story.txt",O_CREAT | O_TRUNC,0644);
+    if (fd == -1){
+      printf("Oh no! Error in opening: %s\n", strerror(errno));
+      return;
+    }
+    //printf("fd is %d\n",fd );
+    close(fd);
+  }
+  else{
+    printf("Already created. Try running ./client or ./control -v\n");
+  }
 }
 
 void view(){
-	char story[4096];
-	int fd = open("stoyr.txt",O_RDONLY);
-	read(fd,story,4096);
-	printf("%s",story);
-	printf("-----END-----\n");
-	close(fd);
+  printf("You have chosen to VIEW the story:\n");
+  int fd = open("story.txt", O_RDONLY);
+  if (fd == -1){
+    printf("Oh no! Error in opening: %s\n", strerror(errno));
+    return;
+  }
+  char * file = calloc(2056, sizeof(char));
+  int last = read(fd, file, 2056);
+  if (last == -1){
+    printf("Oh no! Error in reading: %s\n", strerror(errno));
+    return;
+  }
+  char* text = strrchr(file, '\n');
+  //reset for future use
+  if (text){
+     *text = 0;
+   }
+  printf("%s\n", file);
+
+  close(fd);
+  return;
 }
 
-void removal(){
-	int shmid = shmget(KEY,SHM_SIZE, IPC_CREAT | IPC_EXCL | 0644);
-	shmctl(shmid,IPC_RMID,NULL);
-	//remoce semaphore;
+void delete(){
+  printf("You have chosen to REMOVE the story\n");
+  remove("story.txt");
+  int shmid = shmget(KEY, 100, 0);
+  shmctl(shmid, IPC_RMID, NULL);
+  int semid = semget(KEY, 1, 0);
+  semctl(semid, 0, IPC_RMID);
+  printf("Removed!\n");
 }
-int main(int argc,char *argv){
-	char command[10];
-	if(argc >= 1){
-		strcpy(command,argv[1]);
-	}
-	else{
-		printf("Invalid Input");
-	}
-	command[strlen(command) - 1] = "\0";
-	if(!strcmp("-c",command)){
-		create();
-	}
-	if(!strcmp("-v",command)){
-		view();
-	}
-	if(!strcmp("-r",command)){
-		removal;
-	}			
-	return 0;
+
+int main(int argc, char * argv[]) {
+  union semun {
+    int val;
+    struct semid_ds *buf;
+    unsigned short * array;
+    struct seminfo * __buf;
+  } semaphore;
+  int sem_id;
+
+  if (argc==2) {
+    if (!strcmp(argv[1],"-c")) {
+      create();
+    }
+    else if (!strcmp(argv[1],"-v")) {
+      view();
+    }
+    else if (!strcmp(argv[1],"-r")) {
+      delete();
+    }
+    else
+      printf("Oh no! Your input was wrong. Try ./control -c or ./control -v or ./control -r\n");
+  }
+  else{
+    printf("Oh no! Your input was wrong. Try ./control -c or ./control -v or ./control -r\n");
+  }
+
+  return 0;
 }
